649cce5585c0ddec6e1b641dcce6a57344510144
joemark1989 <joemark1989@gmail.com>
2022-09-12 20:34:38 -0400

    a bit better on the mobile side.

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -74,7 +74,7 @@ export const Navbar: React.FC<NavbarProps> = ({ }) => {
       <Flex flex={1} m="auto" align="center" maxW={800}>
         <NextLink href="/">
           <Link>
-            <Heading size={"lg"} color={textColor}>Reddit Clone</Heading>
+            <Heading size={{base: "sm", sm:"lg"}} color={textColor}>Reddit Clone</Heading>
           </Link>
         </NextLink>
         <Box ml={"auto"}>{body}</Box>
